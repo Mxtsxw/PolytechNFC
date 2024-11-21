2d695a745f1ed04535f69cbf250ea33b9f4f5f88
elkaakourahmad <ahmad.el-kaakour@etu.univ-lyon1.fr>
2024-11-21 01:12:25 +0100

    masquage du mot de passe

--- app/src/main/java/com/polytech/polytechnfc/screens/Sign_in/SignInScreen.kt
+++ app/src/main/java/com/polytech/polytechnfc/screens/Sign_in/SignInScreen.kt
@@ -7,8 +7,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -21,8 +24,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.lint.kotlin.metadata.Visibility
 import androidx.navigation.NavController
+import com.polytech.polytechnfc.R
 import com.polytech.polytechnfc.ViewModel.SignInState
 import com.polytech.polytechnfc.ViewModel.SignInViewModel
 import com.polytech.polytechnfc.model.service.module.AccountServiceImpl
@@ -34,6 +42,7 @@ import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 import org.koin.compose.koinInject
 
+
 @Destination(start = true)
 @Composable
 fun SignInScreen(navigator: DestinationsNavigator,
@@ -43,6 +52,7 @@ fun SignInScreen(navigator: DestinationsNavigator,
 ) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
 
     val signInState by viewModel.signinState.collectAsState()
 
@@ -74,6 +84,18 @@ fun SignInScreen(navigator: DestinationsNavigator,
             label = { Text("Mot de passe") },
             //password = true pour cacher les caractèresf
             singleLine = true,
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(
+                        painter = painterResource(id = if (passwordVisible) R.drawable.ic_visibility else R.drawable.ic_visibility_off),
+                        contentDescription = if (passwordVisible) "Cacher le mot de passe" else "Afficher le mot de passe"
+                    )
+                }
+
+            },
+
+
             modifier = Modifier.fillMaxWidth()
         )
 
