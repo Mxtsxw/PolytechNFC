550a37480f8fd60f60da7e8a9a2fb5cdd6a450f8
Matthieu <randriantsoamatthieu@gmail.com>
2024-11-29 16:24:05 +0100

    refac: CustomListItemComponent and room list

--- app/src/main/java/com/polytech/polytechnfc/screens/RoomsListScreen.kt
+++ app/src/main/java/com/polytech/polytechnfc/screens/RoomsListScreen.kt
@@ -1,6 +1,7 @@
 package com.polytech.polytechnfc.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +10,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -21,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.polytech.polytechnfc.ViewModel.RoomsListViewModel
+import com.polytech.polytechnfc.screens.components.CustomListItemComponent
 import com.polytech.polytechnfc.screens.destinations.HomeScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -34,47 +38,33 @@ fun RoomsListScreen(navigator: DestinationsNavigator,
     val rooms by viewModel.roomsState.collectAsState(emptyList())
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+            .fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Liste des salles",
-            style = MaterialTheme.typography.headlineMedium
-        )
-
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        ){
+            Text(
+                text = "Liste des salles",
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         //Liste des salles
         LazyColumn(
             modifier = Modifier.weight(1f),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             items(rooms) { room ->
-                Card(
-                    modifier = Modifier.fillMaxWidth(),
-                    elevation = CardDefaults.cardElevation(8.dp),
-                    shape = MaterialTheme.shapes.medium
-                ) {
-                    Text(
-                        text = "Salle : ${room.name}",
-                        modifier = Modifier.padding(16.dp),
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                }
+                CustomListItemComponent(
+                    title = room.name,
+                    icon = Icons.Default.Info,
+                    onThreeDotsClick = {},
+                )
             }
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Bouton de retour
-        Button(
-            onClick = { navigator.navigate(HomeScreenDestination) },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Retour")
         }
     }
 }
